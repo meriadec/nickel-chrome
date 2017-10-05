a53fa5218bef1a7112a61929921b831bc3e557ea
meriadec <meriadec.pillet@gmail.com>
2017-10-05 15:21:20 +0200

    correct size

--- src/screenshot.js
+++ src/screenshot.js
@@ -4,7 +4,9 @@ module.exports = async function screenshot(payload) {
   const { html = '', width = 650, height = 650 } = payload
   const browser = await puppeteer.launch({ args: ['--no-sandbox'] })
   const page = await browser.newPage()
+  await page.setViewport({ width, height })
   await page.setContent(html)
   const buffer = await page.screenshot()
+  browser.close()
   return buffer.toString('base64')
 }
